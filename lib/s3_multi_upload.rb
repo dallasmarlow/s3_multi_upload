556decd3c15e90aa804008cc93f5ecb4c9fd5b32
Dallas Marlow <dallas@tumblr-MacBookAir-442a60f69dec.local>
2012-04-21 12:46:31 -0400

    s3

--- lib/s3_multi_upload.rb
+++ lib/s3_multi_upload.rb
@@ -1,33 +1,41 @@
 require 'pathname'
 require 'aws-sdk'
+require 'progress_bar'
 
 module S3_Multi_Upload
   class Upload
+    attr_accessor :options, :file, :s3, :bucket, :object, :progress
 
-    attr_accessor :config, :bucket, :file
+    def initialize options
 
-    def initialize config, bucket, file
-      @file   = Pathname.new file
+      AWS.config :access_key_id     => options[:access_key_id],
+                 :secret_access_key => options[:secret_access_key]
 
-      @config = {
-        :key => @file.basename,
-        :threads    => 5,
-        :chunk_size => 10 * 1024 * 1024, # MB
-      }.merge config
+      @options = options
+      @file    = Pathname.new options[:file]
 
-      AWS.config :access_key_id     => config[:access_key_id],
-                 :secret_access_key => config[:secret_access_key]
+      @s3      = AWS::S3.new
+      @bucket  = @s3.buckets.create options[:bucket]
+      @object  = @bucket.objects[options[:key] || @file.basename]
 
-      @bucket = s3.buckets.create bucket
       enqueue
     end
 
-    def s3
-      @s3 ||= AWS::S3.new
-    end
+    def normalize value, unit = nil
+      case unit.to_sym.downcase
+      when nil, :b, :byte, :bytes
+        value.to_f
+      when :k, :kb, :kilobyte, :kilobytes
+        value.to_f * 1024
+      when :m, :mb, :megabyte, :megabytes
+        value.to_f * 1024 ** 2
+      when :g, :gb, :gigabyte, :gigabytes
+        value.to_f * 1024 ** 3
+      end
+    end 
 
-    def object
-      @object ||= bucket.objects[config[:key]]
+    def chunk_size
+      normalize *options[:chunk_size].first
     end
 
     def queue
@@ -35,8 +43,8 @@ module S3_Multi_Upload
     end
 
     def enqueue
-      (file.size.to_f / config[:chunk_size]).ceil.times do |index|
-        queue << [config[:chunk_size] * index, index + 1]
+      (file.size.to_f / chunk_size).ceil.times do |index|
+        queue << [chunk_size * index, index + 1]
       end
     end
 
@@ -44,14 +52,16 @@ module S3_Multi_Upload
       object.multipart_upload do |upload|
         threads = []
 
-        config[:threads].times do
+        options[:threads].times do
           threads << Thread.new do
             until queue.empty?
               offset, index = queue.deq :asynchronously rescue nil
-
+              
               unless offset.nil?
-                upload.add_part :data => file.read(config[:chunk_size], offset),
+                upload.add_part :data        => file.read(chunk_size, offset),
                                 :part_number => index
+
+                progress.increment! if options[:progress_bar]
               end
             end
           end
@@ -61,13 +71,15 @@ module S3_Multi_Upload
       end
     end
 
+    def progress
+      @progress ||= ProgressBar.new queue.size, :bar, :percentage, :eta
+    end
+
     def process
-      if upload
-        puts "#{file} uploaded to #{config[:bucket]} as #{config[:key]}"
-        exit
-      else
-        abort "upload failed"
-      end
+      value, unit = *options[:chunk_size].first
+      puts "uploading #{file} to s3://#{options[:bucket]}/#{object.key} using #{options[:threads]} threads in chunks of #{value} #{unit}"
+      progress if options[:progress_bar]
+      abort 'upload failed' unless upload
     end
 
   end
