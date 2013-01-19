0bd74bd1b52eae109cbc80829a3ddcbcc641e50b
dallas marlow <dallasmarlow@gmail.com>
2013-01-19 18:27:40 -0500

    just create the queue at init

--- lib/s3_multi_upload.rb
+++ lib/s3_multi_upload.rb
@@ -5,7 +5,7 @@ require 'base64'
 
 module S3_Multi_Upload
   class Upload
-    attr_accessor :options, :file, :s3, :bucket, :object, :progress
+    attr_accessor :options, :file, :queue, :s3, :bucket, :object, :progress
 
     def initialize options
 
@@ -14,6 +14,7 @@ module S3_Multi_Upload
 
       @options = options
       @file    = Pathname.new options[:file]
+      @queue   = Queue.new
 
       @s3      = AWS::S3.new
       @bucket  = @s3.buckets.create options[:bucket]
@@ -37,10 +38,6 @@ module S3_Multi_Upload
 
     def chunk_size
       normalize *options[:chunk_size].first
-    end
-
-    def queue
-      @queue ||= Queue.new
     end
 
     def enqueue
